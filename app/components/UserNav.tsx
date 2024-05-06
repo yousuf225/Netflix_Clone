ca24fda6eaf95e19b810b1142faf9413277e8061
yousuf <man880945@gmail.com>
2024-05-06 14:51:30 +0530

    port change

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -7,6 +7,7 @@ import { signOut } from "next-auth/react";
 
 export default async function UserNav(){
     return(
+        
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
             <Button variant="ghost" className=" relative h-10 w-10 rounded-sm">
@@ -20,7 +21,7 @@ export default async function UserNav(){
             <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel className="flex flex-col space-y-1">
                     <p className=" text-sm font-medium leading-none">RY</p>
-                    <p className=" text-xs leading-none text-muted-foreground">syiyuyh@gmail.com</p>
+                    <p className=" text-xs leading-none text-muted-foreground">syuyt@gmail.com</p>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={()=>signOut()} className=" cursor-pointer">Sign out</DropdownMenuItem>
